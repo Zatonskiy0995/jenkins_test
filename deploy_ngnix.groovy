99a761d6219245cb92449e6d0eec6015a94cf7af
Vladislav570 <Zatonskiy0995@gmail.com>
2026-02-27 19:02:50 +0300

    Добавил визуальную логику

--- deploy_ngnix.groovy
+++ deploy_ngnix.groovy
@@ -1,18 +1,15 @@
-pipeline {
+node {
     agent any
     parameters {
-        string(name: 'PARAMETER_1', defaultValue: '', description: 'Первое вводимое значение')
-        string(name: 'PARAMETER_2', defaultValue: '', description: 'Второе вводимое значение')
+        string(name: 'IP_SERVER', defaultValue: '213.183.53.195', description: 'Ведите ip адрес')
+        string(name: 'DOCKER_KONTEINER', defaultValue: 'objective_bose', description: 'Ведите имя контейнера на сервере "docker ps"')
     }
     stages {
-        stage('Test Stage') {
+        stage('Вывод Задачи') {
             steps {
                 echo "Привет, мир! Jenkins работает."
-                echo "Значение первого параметра: ${params.PARAMETER_1}"
-                echo "Значение второго параметра: ${params.PARAMETER_2}"
-                
-                sh 'env'
-                sh 'ansible --version'
+                echo "Значение первого параметра: ${params.IP_SERVER}"
+                echo "Значение второго параметра: ${params.DOCKER_KONTEINER}"
             }
         }
         stage('Cleanup') {
