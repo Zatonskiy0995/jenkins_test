a1f2d6eb8320c903506377a3cd6b95122361228a
Vladislav570 <Zatonskiy0995@gmail.com>
2026-02-27 19:05:19 +0300

    fix

--- deploy_ngnix.groovy
+++ deploy_ngnix.groovy
@@ -1,21 +1,21 @@
-node {
-    agent any
-    parameters {
-        string(name: 'IP_SERVER', defaultValue: '213.183.53.195', description: 'Ведите ip адрес')
-        string(name: 'DOCKER_KONTEINER', defaultValue: 'objective_bose', description: 'Ведите имя контейнера на сервере "docker ps"')
-    }
-    stages {
-        stage('Вывод Задачи') {
-            steps {
-                echo "Привет, мир! Jenkins работает."
-                echo "Значение первого параметра: ${params.IP_SERVER}"
-                echo "Значение второго параметра: ${params.DOCKER_KONTEINER}"
-            }
-        }
-        stage('Cleanup') {
-            steps {
-                cleanWs()
-            }
-        }
-    }
+pipeline {
+      agent any
+      parameters {
+          string(name: 'IP_SERVER', defaultValue: '213.183.53.195', description: 'Введите IP адрес сервера')
+          string(name: 'DOCKER_CONTAINER', defaultValue: 'objective_bose', description: 'Введите имя Docker-контейнера на сервере ("docker ps")')
+      }
+      stages {
+          stage('Вывод Задачи') {
+              steps {
+                  echo "Привет, мир! Jenkins работает."
+                  echo "IP-адрес сервера: ${params.IP_SERVER}"
+                  echo "Имя Docker-контейнера: ${params.DOCKER_CONTAINER}"
+              }
+          }
+          stage('Cleanup') {
+              steps {
+                  cleanWs()
+              }
+          }
+      }
 }
