685cfbba8895ab5edb881bf6af25a996a5a767f7
Vladislav570 <Zatonskiy0995@gmail.com>
2026-02-27 11:39:51 +0300

    sss

--- main.groovy
+++ main.groovy
@@ -1,27 +1,13 @@
-import hudson.model.*
-import org.jenkinsci.plugins.workflow.job.WorkflowJob
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
-
-// Получаем экземпляр Jenkins
-def instance = Jenkins.getInstance()
-
-// Название задания
-String jobName = "MyFirstPipeline"
-
-// Определяем содержимое нашей Pipeline
-String pipelineScript = """
 node {
-     stage('Hello') {
-        echo 'Привет от Jenkins!'
-     }
+      stage('Test Stage') {
+          // Простое эхо-командование для отображения результата
+          echo 'Привет, мир! Jenkins работает.'
+          
+          // Проверка окружения
+          sh 'env'
+      }
+      
+      stage('Cleanup') {
+          cleanWs()
+      }
 }
-"""
-
-// Создаем новое задание типа WorkflowJob
-WorkflowJob newJob = instance.createProject(WorkflowJob.class, jobName)
-newJob.setDefinition(new CpsFlowDefinition(pipelineScript, true))
-
-// Сохраняем изменения
-instance.save()
-
-println("Задание ${jobName} создано.")
