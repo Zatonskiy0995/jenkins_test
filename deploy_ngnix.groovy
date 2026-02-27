0464e1e24607b10506536861e3c8a615191a53c2
Vladislav570 <Zatonskiy0995@gmail.com>
2026-02-27 18:57:51 +0300

    Добавил вывод

--- deploy_ngnix.groovy
+++ deploy_ngnix.groovy
@@ -1,19 +1,24 @@
-node {
+pipeline {
     agent any
-        parameters {
-            string(name: 'IP_SEREVER', defaultValue: '', description: 'ip сервера')
-            string(name: 'DOCER_NAME', defaultValue: '', description: 'Название контейнера')
+    parameters {
+        string(name: 'PARAMETER_1', defaultValue: '', description: 'Первое вводимое значение')
+        string(name: 'PARAMETER_2', defaultValue: '', description: 'Второе вводимое значение')
     }
-    stage('Test Stage') {
-        // Простое эхо-командование для отображения результата
-        echo 'Привет, мир! Jenkins работает.'
-        
-        // Проверка окружения
-        sh 'env'
-        sh 'ansible --version'
-    }
-    
-    stage('Cleanup') {
-        cleanWs()
+    stages {
+        stage('Test Stage') {
+            steps {
+                echo "Привет, мир! Jenkins работает."
+                echo "Значение первого параметра: ${params.PARAMETER_1}"
+                echo "Значение второго параметра: ${params.PARAMETER_2}"
+                
+                sh 'env'
+                sh 'ansible --version'
+            }
+        }
+        stage('Cleanup') {
+            steps {
+                cleanWs()
+            }
+        }
     }
 }
